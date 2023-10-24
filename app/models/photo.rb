4302e51b2769392e59e00eca002cc0467f76169c
David Hibble <139304207+hibbleD@users.noreply.github.com>
2023-10-24 20:14:18 +0000

    Added validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -21,11 +21,14 @@
 #
 class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", counter_cache: true
+
+  has_many :comments
+
+  has_many :likes
+
+  has_many :fans, through: :likes
+
+  validates :caption, presence: true 
+
+  validates :image, presence: true
 end
-
-
-has_many :comments,
-
-has_many :likes
-
-has_many :fans, through: :likes
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_24_165056) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_24_172817) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -62,7 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_24_165056) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "username"
-    t.boolean "private"
+    t.boolean "private", default: true
     t.integer "likes_count", default: 0
     t.integer "comments_count", default: 0
     t.datetime "created_at", null: false
