e87cca58333ccc738f1aa464824c0096717f3b14
David Hibble <139304207+hibbleD@users.noreply.github.com>
2023-10-25 16:36:01 +0000

    - Added sample data

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -22,11 +22,11 @@
 class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", counter_cache: true
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
-  has_many :likes
+  has_many :likes, dependent: :destroy
 
-  has_many :fans, through: :likes
+  has_many :fans, through: :likes, dependent: :destroy
 
   validates :caption, presence: true 
 
