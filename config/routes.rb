3f2063c300a994d23d3a4be3a30001bc4aad1431
David Hibble <139304207+hibbleD@users.noreply.github.com>
2023-10-25 17:10:35 +0000

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "photos#index"
+
+  devise_for :users
+
+  #resources :likes
+  #resources :follow_requests
+  #resources :comments
+  #resources :photos
 
   # Defines the root path route ("/")
   # root "articles#index"
