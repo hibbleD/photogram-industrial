ead9e75187ebd88b77972c14b2fa1564a705cc77
David Hibble <139304207+hibbleD@users.noreply.github.com>
2023-10-26 21:08:10 +0000

    Changed Users route

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   root "photos#index"
 
-  get "/users/:id" => "users#show", as: :user
-
+  
   
   devise_for :users
   
   resources :likes
   resources :follow_requests
   resources :comments
-  resources :users, only: :show
   resources :photos
+
+  get "/:username" => "users#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
