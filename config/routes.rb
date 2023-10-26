de8d2860d4590c39ea42a28a8f65845350896cae
David Hibble <139304207+hibbleD@users.noreply.github.com>
2023-10-26 16:49:20 +0000

    Set up user show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root "photos#index"
 
-  devise_for :users
+  get "/users/:id" => "users#show", as: :user
 
+  
+  devise_for :users
+  
   resources :likes
   resources :follow_requests
   resources :comments
+  resources :users, only: :show
   resources :photos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
