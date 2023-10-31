9c4f3bbcb4d47fe2cff1567807b4060908fc4a32
David Hibble <139304207+hibbleD@users.noreply.github.com>
2023-10-31 17:09:36 +0000

    Finish profile bar

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,10 @@ Rails.application.routes.draw do
   resources :comments
   resources :photos
 
-  get ":username/liked" => "photos#liked", as: :liked_photos
+  get ":username/liked" => "users#liked", as: :liked
+  get ":username/feed" =>  "users#feed", as: :feed
+  get ":username/followers" =>  "users#followers", as: :followers
+  get ":username/following" =>  "users#following", as: :following
 
   get "/:username" => "users#show", as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
